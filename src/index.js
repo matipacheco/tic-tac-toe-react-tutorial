f64badf3431684d9ed64d36c13244fd1be5dd3b7
Mati <matipacheco@gmail.com>
2019-03-21 16:21:39 -0300

    refactoring Square. Now it is a controlled component

--- src/index.js
+++ src/index.js
@@ -3,25 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        }
-    }
-
     render() {
         return (
-            <button className="square" onClick={() => this.setState({value: 'X'})}>
-                {this.state.value}
+            <button className="square" onClick={() => this.props.onClick()}>
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
-    static renderSquare(i) {
-        return <Square value={i}/>;
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
+    renderSquare(i) {
+        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
     }
 
     render() {
@@ -31,19 +37,19 @@ class Board extends React.Component {
             <div>
                 <div className="status">{status}</div>
                 <div className="board-row">
-                    {Board.renderSquare(0)}
-                    {Board.renderSquare(1)}
-                    {Board.renderSquare(2)}
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
                 </div>
                 <div className="board-row">
-                    {Board.renderSquare(3)}
-                    {Board.renderSquare(4)}
-                    {Board.renderSquare(5)}
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
                 </div>
                 <div className="board-row">
-                    {Board.renderSquare(6)}
-                    {Board.renderSquare(7)}
-                    {Board.renderSquare(8)}
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
                 </div>
             </div>
         );
